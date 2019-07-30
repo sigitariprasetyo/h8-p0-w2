be680bd2c067c1db2b04a60103e0acb02cce460a
Sigit Ari Prasetyo <sigitari111@gmial.com>
2019-07-30 08:45:42 +0700

    edit exercises3.js

--- exercises3.js
+++ exercises3.js
@@ -1,22 +1,24 @@
 
 //[Exercises3] Mengenal penggunaan Conditional (if-else)
 
-var nama = 'sigit'
-var peran = 'penyihir' //pilihan peran : ksatria, tabib, penyihir
+var nama = 'sigit';
+var peran = 'asa'; //pilihan peran : ksatria, tabib, penyihir
 
 if(nama === '' && peran === '' ){
-    console.log('Nama harus di isi!')
+    console.log('Nama harus di isi!');
 } else if(nama === 'sigit' && peran === ''){
-    console.log('Hallo ' + nama + ', Pilih peranmu untuk memulai game!')
+    console.log('Hallo ' + nama + ', Pilih peranmu untuk memulai game!');
 } else if (nama === 'sigit' && peran === 'ksatria'){
-    console.log('Selamat datang di Dunia Proxytia, ' + nama)
-    console.log('Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu!')
+    console.log('Selamat datang di Dunia Proxytia, ' + nama);
+    console.log('Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu!');
 } else if (nama === 'sigit' && peran === 'tabib'){
-    console.log('Selamat datang di Dunia Proxytia, ' + nama)
-    console.log('Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka.')
-}else if (nama === 'sigit' && peran === 'penyihir'){
-    console.log('Selamat datang di Dunia Proxytia, ' + nama)
-    console.log('Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!')
+    console.log('Selamat datang di Dunia Proxytia, ' + nama);
+    console.log('Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka.');
+} else if(nama === 'sigit' && peran === 'penyihir'){
+    console.log('Selamat datang di Dunia Proxytia, ' + nama);
+    console.log('Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
+} else {
+    console.log('Peran yang dipilih tidak sesuai!');
 }
 
 
