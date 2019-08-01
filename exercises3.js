71c2e2d4f0c9352c65c4cd2a09b030315c91464d
Sigit Ari Prasetyo <sigitari111@gmial.com>
2019-08-01 21:08:23 +0700

    edit exercises3.js

--- exercises3.js
+++ exercises3.js
@@ -1,20 +1,20 @@
 
 //[Exercises3] Mengenal penggunaan Conditional (if-else)
 
-var nama = 'sigit';
-var peran = 'asa'; //pilihan peran : ksatria, tabib, penyihir
+var nama = 'ari';
+var peran = ''; //pilihan peran : ksatria, tabib, penyihir
 
-if(nama === '' && peran === '' ){
+if(nama === '' && peran !== '' ){
     console.log('Nama harus di isi!');
-} else if(nama === 'sigit' && peran === ''){
+} else if(nama !== '' && peran === ''){
     console.log('Hallo ' + nama + ', Pilih peranmu untuk memulai game!');
-} else if (nama === 'sigit' && peran === 'ksatria'){
+} else if (nama !== '' && peran === 'ksatria'){
     console.log('Selamat datang di Dunia Proxytia, ' + nama);
     console.log('Halo Ksatria ' + nama + ', kamu dapat menyerang dengan senjatamu!');
-} else if (nama === 'sigit' && peran === 'tabib'){
+} else if (nama !== '' && peran === 'tabib'){
     console.log('Selamat datang di Dunia Proxytia, ' + nama);
     console.log('Halo Tabib ' + nama + ', kamu akan membantu temanmu yang terluka.');
-} else if(nama === 'sigit' && peran === 'penyihir'){
+} else if(nama !== '' && peran === 'penyihir'){
     console.log('Selamat datang di Dunia Proxytia, ' + nama);
     console.log('Halo Penyihir ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!');
 } else {
