24efeb5cf20e2a56718ffd0a2af4a5af9847ba1b
Sigit Ari Prasetyo <sigitari111@gmial.com>
2019-08-01 21:45:17 +0700

    edit exercises13.js

--- exercises13.js
+++ exercises13.js
@@ -1,22 +1,22 @@
 function xo(str) {
   // you can only write your code here!
-  var x = '';
-  var o = '';
-  var hasil;
+var x = '';
+var o = '';
+var hasil;
 
-  for(i = 0; i < str.length; i++){
-      if(str[i] == 'x'){
-          x += 'x';
-      } else {
-          o += '0';
-      }
-  }
-  if(x.length === o.length){
-      hasil = 'true';
-  } else {
-      hasil = 'false';
-  }
-  return hasil;
+for(i = 0; i < str.length; i++){
+    if(str[i] == 'x'){
+        x += 'x';
+    } else {
+        o += '0';
+    }
+}
+if(x.length === o.length){
+    hasil = true;
+} else {
+    hasil = false;
+}
+return hasil;
 }
 
 // TEST CASES
